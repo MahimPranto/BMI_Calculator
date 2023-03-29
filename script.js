9416e6001e688434ae7ea0d820eeced80f4cd368
MahimPranto <mahim.pranto@gmail.com>
2023-03-29 18:39:36 +0600

    final update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function calculateBMI(){
         document.querySelector('.result').innerHTML ="Invalid Input";
     else if(bmi<18.5)
         document.querySelector('.result').innerHTML ="Your BMI is: "+bmi+" (Underweight)";
-    else if (bmi>=18.5 && bmi<=24.9)
+    else if (bmi>=18.5 && bmi<25)
         document.querySelector('.result').innerHTML ="Your BMI is: "+bmi+" (Healthy)";
     else if (bmi>=25 && bmi<=29.9)
         document.querySelector('.result').innerHTML ="Your BMI is: "+bmi+" (Overweight)";
